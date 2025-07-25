343a7ef4324f3d7db0ba6c5ac6208034388ad0e4
darkwolf999super <randallchrist56@gmail.com>
2025-07-25 05:38:51 -0700

    fix build

--- app/page.js
+++ app/page.js
@@ -1,3 +1,5 @@
+'use client';
+
 import AboutSection from "./components/homepage/about";
 import ContactSection from "./components/homepage/contact";
 import Education from "./components/homepage/education";
@@ -6,7 +8,7 @@ import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
 
-export default async function Home() {
+export default function Home() {
   return (
     <>
       <HeroSection />
@@ -17,5 +19,5 @@ export default async function Home() {
       <Education />
       <ContactSection />
     </>
-  )
-};
+  );
+}
